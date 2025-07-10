e004783baad44b5e621c9a9d4ada1a515ba81373
kauafodaomaneiro <kaua_r_ribeiro@estudante.sesisenai.org.br>
2025-07-10 09:54:44 -0300

    ADD - exercicio04 finalizado

--- exercicio04.php
+++ exercicio04.php
@@ -6,5 +6,31 @@
 </head>
 <body>
     
+<form method="POST" action="">
+        <label for="fatorial">Calcula o fatorial do número:</label>
+        <input type="number" id="fatorial" name="fatorial" required>
+        <button type="submit" name="calcularfatorial">fatorial</button>
+    </form>
+
+    <?php
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        if(isset($_POST['fatorial'])){
+            $numero = $_POST['fatorial'];
+            $resultado = 1;
+            
+            for($i = $numero; $i >= 1; $i--){
+                $resultado *= $i;
+                
+                 }
+            echo $resultado;
+            echo '<br>';
+
+        };
+    };
+    
+    ?>
+
 </body>
 </html>
