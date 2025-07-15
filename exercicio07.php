5cc98105445d653fc7a9912680a2467b69caac4e
kaua <kaua_r_ribeiro@estudante.sesisenai.org.br>
2025-07-15 11:15:57 -0300

    ADD - exercicio07 finalizado

--- exercicio07.php
+++ exercicio07.php
@@ -6,5 +6,41 @@
 </head>
 <body>
     
+<form method="POST" action="">
+        <label for="num">informe o numero:</label>
+        <input type="number" id="num" name="num" required>
+        <button type="submit" name="verificar_perfeito">Verificar perfeito</button>
+    </form>
+
+    <?php
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if(isset($_POST['num'])){
+            $numero = $_POST['num'];
+            $soma = 0;
+
+            for($i = ($numero-1); $i >= 1; $i-- ){
+                if($numero % $i ==0){
+                    $divisores = $i;
+
+                    $soma += $divisores;
+
+                };
+
+        
+            };
+            if($soma == $numero){
+                        echo "O numero $numero é perfeito";
+                    }else{
+                        echo "O numero $numero não é perfeito";
+                    };
+        };
+
+    };
+
+    ?>
+
+
+
 </body>
 </html>
